3a055277a08a42dfdb73965ba6bd585c4ceb5b2c
unknown <ndrurufrangklynbf@gmail.com>
2025-01-03 22:37:16 +0700

    refactor : refactor getBookById

--- config/routes.js
+++ config/routes.js
@@ -15,21 +15,27 @@ const { db } = require('./firebase_config.js')
 const router = require('express').Router()
 
 const bodyParser = require('body-parser')
-const { v4: uuidv4 } = require("uuid")
-const { getAllBook, } = require('../controller/book_controller.js')
+
+const {
+    getAllBook,
+    getBookById,
+    addBook,
+} = require('../controller/book_controller.js')
 
 
 
 router.use(bodyParser.json());
 
 router.get('/getAllBook', async(req, res) => {
-    // console.log("ini line 26")
 
+    try {
+        const result = await getAllBook();
+        console.log(result)
+        res.send(result);
 
-    const result = await getAllBook();
-    console.log(result)
-        // res.status(result.statusCode).send(result.data);
-    res.send(result);
+    } catch (error) {
+        res.send(error)
+    }
 
 });
 
@@ -38,49 +44,27 @@ router.get('/bookData/:id', async(req, res) => {
 
     try {
         const { id } = req.params;
-        const data = doc(db, 'book_library', id);
-        const book_data = await getDoc(data);
 
-        console.log("ini line 55 book data", id)
-        console.log(book_data.data())
+        const result = await getBookById(req);
 
-        if (book_data.exists()) {
-
-            console.log("Document data : ", book_data.data());
-        } else {
-            // docSnap.data() will be undefined in this case
-            // console.log("No such document!");
-            res.status(404).send({ message: "No such document!" })
-        }
-
-        res.send(book_data.data())
+        console.log("Data : ", result);
+        res.send(result)
 
     } catch (error) {
-        console.log("ini error /line 52", error)
-        res.status(404).send(error)
+        res.send(error)
     }
-
 
 })
 
 router.post('/library', async(req, res) => {
-    const book_id = uuidv4()
 
-    console.log(book_id)
-    const docData = {
-
-        title: req.body.title,
-        bookName: req.body.bookName,
-        datePublished: req.body.datePublished,
-        lastUpdate: Date.now(),
-
+    try {
+        const result = await addBook(req);
+        console.log("Data : ", result);
+        res.send(result)
+    } catch (error) {
+        res.send(error)
     }
-    console.log("ini line 62")
-    const docRef = await addDoc(collection(db, "book_library"), docData);
-
-    console.log(docData)
-
-    res.send({ status: "Success!" })
 });
 
 
--- controller/book_controller.js
+++ controller/book_controller.js
@@ -1,42 +1,107 @@
 const {
+
+    addDoc,
     collection,
     getDocs,
-    getDoc
+    getDoc,
+    doc
 } = require('firebase/firestore')
 
 const { db } = require('../config/firebase_config.js');
+
+const { v4: uuidv4 } = require("uuid")
 const { messaging } = require('firebase-admin');
 
+let result_data = {
+    statusCode: undefined,
+    message: "",
+    data: []
+}
 
 async function getAllBook() {
-    // console.log("ini db", db);
+
     const book_data = await getDocs(collection(db, 'book_library'))
 
-    let result_data = {
-        statusCode: undefined,
-        message: "",
-        data: []
-    }
+    if (book_data) {
 
-    book_data.forEach((book) => {
-        if (book.exists()) {
+        result_data.statusCode = 200
+        result_data.message = "Successful!"
+        result_data.data = book_data.docs.map(doc => doc.data());
 
-            result_data.statusCode = 200,
-                result_data.data = book.data()
-            result_data.message = "Successful!"
+    } else {
 
-        } else {
-            // docSnap.data() will be undefined in this case
+        result_data.statusCode = 404
+        result_data.message = "No such document!"
 
-            result_data.statusCode = 404,
-                result_data.message = "No such document!"
-        };
-
-    });
+    };
     return result_data;
 };
 
+async function getBookById(req, res) {
+
+    const { id } = req.params;
+
+    const data = doc(db, 'book_library', id);
+
+    console.log(`This is the data from "${id}"`)
+    const book_data = await getDoc(data);
+
+    if (book_data.exists()) {
+
+        result_data.statusCode = 200
+        result_data.data = book_data.data()
+        result_data.message = `Successful to get data ${id}!`
+
+    } else {
+
+        result_data.statusCode = 404
+        result_data.message = "No such document!"
+    }
+
+    return result_data;
+
+}
+
+async function addBook(req, res) {
+    const book_id = uuidv4()
+
+    console.log(book_id)
+    const docData = {
+
+        title: req.body.title,
+        bookName: req.body.bookName,
+        datePublished: req.body.datePublished,
+        lastUpdate: Date.now(),
+
+    }
+    console.log("ini line 77")
+    const docRef = await addDoc(collection(db, "book_library"), docData);
+    console.log("ini line 79")
+
+
+    if (docRef) {
+        result_data.statusCode = 200
+        result_data.data = docRef.data()
+        console.log("ini line 85");
+        console.log(result_data.data);
+        result_data.message = `Successful to add "${book_id}"!`
+    } else {
+        result_data.statusCode = 404
+        result_data.message = "Add book failed!"
+
+    }
+    return result_data.data[{
+        book_id,
+        title: title,
+        bookName: bookName,
+        datePublished: datePublished,
+
+    }];
+}
+
 module.exports = {
     getAllBook,
+    getBookById,
+    addBook,
 
 }
